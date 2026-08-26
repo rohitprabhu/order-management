a5f5d534a47b3390cc13e65c392eaaa16786de90
rohitprabhu <rohitprabhu1012@gmail.com>
2026-08-26 08:49:06 +0530

    Added more products to seed the database

--- database/seeders/data/product_listings.php
+++ database/seeders/data/product_listings.php
@@ -30,5 +30,70 @@ return [
         "name"  => "Massage Gun",
         "price" => 700,
         "stock_quantity" => '0'
+    ],
+    [
+        "name"  => "Wireless Mouse",
+        "price" => 499,
+        "stock_quantity" => '45'
+    ],
+    [
+        "name"  => "Bluetooth Speaker",
+        "price" => 1299,
+        "stock_quantity" => '20'
+    ],
+    [
+        "name"  => "Cotton T-Shirt",
+        "price" => 399,
+        "stock_quantity" => '100'
+    ],
+    [
+        "name"  => "Stainless Steel Water Bottle",
+        "price" => 599,
+        "stock_quantity" => '60'
+    ],
+    [
+        "name"  => "Notebook",
+        "price" => 99,
+        "stock_quantity" => '250'
+    ],
+    [
+        "name"  => "Ballpoint Pen (Pack of 5)",
+        "price" => 49,
+        "stock_quantity" => '500'
+    ],
+    [
+        "name"  => "Backpack",
+        "price" => 899,
+        "stock_quantity" => '40'
+    ],
+    [
+        "name"  => "Smartphone Stand",
+        "price" => 199,
+        "stock_quantity" => '150'
+    ],
+    [
+        "name"  => "LED Desk Lamp",
+        "price" => 799,
+        "stock_quantity" => '25'
+    ],
+    [
+        "name"  => "Coffee Mug",
+        "price" => 249,
+        "stock_quantity" => '80'
+    ],
+    [
+        "name"  => "Yoga Mat",
+        "price" => 699,
+        "stock_quantity" => '35'
+    ],
+    [
+        "name"  => "Gaming Headset",
+        "price" => 1499,
+        "stock_quantity" => '15'
+    ],
+    [
+        "name"  => "USB-C Cable",
+        "price" => 299,
+        "stock_quantity" => '200'
     ]
 ];
